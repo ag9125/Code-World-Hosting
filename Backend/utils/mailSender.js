daef12d97513ec7e0563a993aa4f0740db76ac86
Ankit Gupta <147823078+ag9125@users.noreply.github.com>
2026-07-20 18:32:32 +0530

    Add port configuration to mail transporter

--- Backend/utils/mailSender.js
+++ Backend/utils/mailSender.js
@@ -4,6 +4,8 @@ const sendMail=async (mail,title,body)=>{
     try{
         let transporter = nodemailer.createTransport({
             host:process.env.MAIL_HOST,
+             port: Number(process.env.MAIL_PORT),
+    secure: false,
             auth:{
                 user:process.env.MAIL_USER,
                 pass:process.env.MAIL_PASSWORD
